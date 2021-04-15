78cf34e042545c37f7a5b88b750382423f861edc
SalvoCalcagno <s.calcagno95@gmail.com>
2021-04-15 09:37:05 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+#
+
+from setuptools import setup
+
+setup(name='eremus',
+      version='0.0.1',
+      description='utilities for EREMUS dataset',
+      license='GPLv3',
+      packages=['eremus'],
+      zip_safe=False)
